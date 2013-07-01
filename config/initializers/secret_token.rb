804c1bdac11f6826321bf2678d08ce4b658c86c8
Palazzini Francesco <palazzini.francesco@gmail.com>
2013-07-02 00:39:34 +0200

    empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Invoicer::Application.config.secret_token = 'be1fe081015fb1dbb9a3c961cf1408aff02c1acfa4a9234cf356cc8b5f5c604c07b124496dd5494e244fecc93b311807feaf1d920288174bcabf18196d9d0656'
